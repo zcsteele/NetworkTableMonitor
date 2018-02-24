58d00bb30bad9cf88c0b25fcc106a27f0e9f86ec
Zach Steele <zcsteele@gmail.com>
2018-02-24 00:38:15 -0800

    Duplicated build scripts from RaspberryPi project to get NetworkTables interface (old build backed up as build.gradle.backup)

--- src/main/java/org/snobotics/NetworkTable/Monitor.java
+++ src/main/java/org/snobotics/NetworkTable/Monitor.java
@@ -1,5 +1,8 @@
 package org.snobotics.NetworkTable;
 
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableInstance;
+
 /*
  * Monitor the NetworkTables activity, displaying active tables/data
  *
@@ -9,7 +12,15 @@ public class Monitor {
         return "Hello world.";
     }
 
+    public static NetworkTableInstance testStatic =
+        NetworkTableInstance.getDefault();
+
     public static void main(String[] args) {
+        System.loadLibrary("ntcore");
+        NetworkTableInstance netTable =
+              NetworkTableInstance.getDefault();
+//        testStatic.startServer();
+        netTable.startServer();
         System.out.println(new Monitor().getGreeting());
     }
 }
